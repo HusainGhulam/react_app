226740837900d6802d402c61bbe774df2563fff7
husaindev <husainalimca@gmail.com>
2024-12-28 18:57:35 +0530

    todo react app

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+//import NetflixSeries2 from "./components/NetflixSeries2";
+//import "./components/Ntflix.css";
+//import "./components/Ntflix.module.css";
+// import {EventHandling}  from "./components/EventHandling";
+//import { EventProps } from "./components/EventProps";
+//import { EventPropagation } from "./components/EventPropagation";
+//import { State } from "./components/Hooks/State";
+//import {DerivedState} from "./components/DerivedState"
+
+import { Todo } from "./projects/Todo/Help";
+
+//import { ToggleSwitch } from "./projects/ToggleSwitch";
+
+//import { LiftStateUp } from "./components/LiftStateUp";
+export const App = () => {
+  return (
+    <>
+      <section>
+        {/* <h1 className="common-heading">This is card heading</h1>
+      <NetflixSeries2 /> */}
+        {/* <EventHandling /> */}
+        {/* <EventProps /> */}
+        {/* <EventPropagation /> */}
+        {/* <State /> */}
+        {/* <DerivedState /> */}
+        {/* <LiftStateUp /> */}
+        {/* <ToggleSwitch /> */}
+        <Todo />
+      </section>
+    </>
+  );
+};
